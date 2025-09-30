005e4cde724fe72ed61b2a13c09a7159677f2128
evilssouls <evilssouls@protonmail.com>
2025-10-01 00:38:29 +0200

    Changed a bunch of configs

--- neovim/nvim/lua/config/keybindings.lua
+++
@@ -1,1 +0,0 @@
--- TODO: nohl, CTRL-O replaced with HL, Enter and Shift+Enter = Newline above and below
---
+++ neovim/nvim/lua/config/remap.lua
@@ -0,0 +1,11 @@
+-- TODO: nohl, CTRL-O replaced with HL, Enter and Shift+Enter = Newline above and below
+vim.keymap.set("", "<leader>nh", ":nohl<CR>")
+-- vim.keymap.set("n", "<Enter>", ":call append(line('.'), '')<CR>")
+vim.keymap.set("n", "<Enter>", function() vim.fn.append(vim.fn.line('.'), '') vim.cmd('norm! j') end)
+vim.keymap.set("n", "<S-Enter>", function() vim.fn.append(vim.fn.line('.')-1, '') vim.cmd('norm! k') end)
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
